44f977b8a77ece4ef5608d019c4fbe49bb78a118
Ben Morton <benmorton28@yahoo.com>
2017-02-01 17:41:01 +0900

    first commmit of single year functions

--- R/get_enrollment.R
+++ R/get_enrollment.R
@@ -1,11 +1,11 @@
-#' @title Get Price
-#' @description Gets the tuition price from an archived Williams Course Catalog PDF link
+#' @title Get Enrollment
+#' @description Gets the enrollment by year(at the beginning of the academic year) from an archived Williams Course Catalog PDF link
 #' @param link A link to a yearly pdf file on the Williams College Registrar's website.
-#' @return Price of tuition for that academic year
+#' @return A numeric vector containing enrollment by year, from graduate, and then senior to freshman.
 #' @importFrom pdftools pdf_text
 #' @importFrom string str_replace_all
 #' @export
-get_price <- function(link= "http://web.williams.edu/admin/registrar//catalog/catalog0910.pdf"){
+get_enrollment <- function(link= "http://web.williams.edu/admin/registrar//catalog/catalog0910.pdf"){
   download_path <- paste("./inst/extdata/", basename(link), sep = "")
   if(!file.exists(download_path)){
     download.file(link, destfile = download_path)
@@ -14,9 +14,8 @@ get_price <- function(link= "http://web.williams.edu/admin/registrar//catalog/ca
   concatenated_text <- paste(text, collapse = "")
   replaced_text <- stringr::str_replace_all(concatenated_text, "[\n]" , " ")
   replaced_text <- gsub("\\s+"," ",replaced_text)
-  replaced_text <- gsub(".*(College charges)","",replaced_text)
+  replaced_text <- gsub(".*(BY CLASSES)","",replaced_text)
   word_list <- strsplit(replaced_text, split = ' ')[[1]]
-  cost <- word_list[grep('[$].*', word_list)[1]]
-  cost <- as.numeric(substring(gsub(',', '', cost), 2, nchar(cost)))
-  return(cost)
+  cost <- word_list[grep('^[0-9]+$', word_list)]
+  return(as.numeric(word_list[grep('^[0-9]+$', word_list)[c(2,4,6,8,10)]]))
 }
--- R/get_geographical_distribution.R
+++ R/get_geographical_distribution.R
@@ -1,15 +1,33 @@
 #' @title Get Geographical Distribution
 #' @description Gets the geographical distribution data from a archived Williams Course Catalog PDF link.
 #' @param link A link to a yearly pdf file on the Williams College Registrar's website.
-#' @return A dataframe with two columns:country and count
+#' @return A list of size two with two dataframes(one domestic, one international) with two columns:state/country and number.
 #' @importFrom pdftools pdf_text
-#' @importFrom string str_replace_all
+#' @importFrom string str_replace_all str_trim str_extract_all
 #' @export
-download_data <- function(link= "http://web.williams.edu/admin/registrar//catalog/catalog0910.pdf"){
-  download.file(link, destfile = "./catalog0910.pdf")
-  text <- pdftools::pdf_text("./catalog0910.pdf")
+get_geographical_distribution <- function(link= "http://web.williams.edu/admin/registrar//catalog/catalog0910.pdf"){
+  download_path <- paste("./inst/extdata/", basename(link), sep = "")
+  if(!file.exists(download_path)){
+    download.file(link, destfile = download_path)
+  }
+  text <- pdftools::pdf_text(download_path)
   concatenated_text <- paste(text, collapse = "")
   replaced_text <- stringr::str_replace_all(concatenated_text, "[\n]" , " ")
-  replaced_text_2 <- gsub("\\s+"," ",replaced_text)
-  tokenized_text <- strsplit(replaced_text_2, " ")[[1]]
+  replaced_text <- gsub("\\s+"," ",replaced_text)
+  replaced_text <- gsub(".*(GEOGRAPHICAL DISTRIBUTION)","",replaced_text)
+  replaced_text <- gsub("(INDEX).*","",replaced_text)
+  domestic <- gsub(".*(U.S. )","",replaced_text)
+  domestic <- stringr::str_trim(gsub("(International).*","",domestic))
+  domestic <- str_extract_all(domestic, "[A-Z][^0-9]+[0-9]+")[[1]]
+  domestic_names <- stringr::str_replace_all(domestic, "[0-9]+" , "")
+  domestic_numbers <- stringr::str_replace_all(domestic, "[^0-9]+" , "")
+  domestic_df <- data.frame(domestic_names, domestic_numbers)
+  names(domestic_df) <- c("State","Number")
+  international <- stringr::str_trim(gsub(".*(International)","",replaced_text))
+  international <- str_extract_all(international, "[A-Z][^0-9]+[0-9]+")[[1]]
+  international_names <- stringr::str_replace_all(international, "[0-9]+" , "")
+  international_numbers <- stringr::str_replace_all(international, "[^0-9]+" , "")
+  international_df <- data.frame(international_names, international_numbers)
+  names(international_df) <- c("Country","Number")
+  return(list(domestic_df, international_df))
 }
